b1cd952e7c45fee16c02311550ef777aed18612e
Sayah El Yatim <sayah@elyat.im>
2020-12-29 10:04:44 +0100

    Add "exemple c" and style.css

--- exercices/index.php
+++ exercices/index.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html>
+
+
+
 <body>
 
 <h1>Exercice 1 de PHP</h1>
@@ -14,13 +17,15 @@
 <h2>Exemple b)</h2>
 
 <?php
+    // Définition de variable
     $texteDuParagraphe = "Hello World!";
     $entier = 7;
     $decimal = 10.5;
-    $x = "12.5io1"; // reconnait les premiers caractères comme étant un type decimal
+    $x = 12.5; // reconnait les premiers caractères comme étant un type decimal
     $y = 4;
     $z = $x + $decimal;
 
+    // Définition de constante
     define("VITESSE_MAX", 300000);
     define("AGE_LIMITE", 14);
 
@@ -29,12 +34,43 @@
     // echo $decimal;
 
     // echo $VITESSE_MAX; est une erreur car ce n'est PAS une VARIABLE mais une CONSTANTE
-    echo VITESSE_MAX;
-    echo " ";
-    echo constant("VITESSE_MAX");
+    // echo VITESSE_MAX;
+    // echo " ";
+    // echo constant("VITESSE_MAX");
+
+    echo $z;
 ?>
 
 <h2>Exemple c)</h2>
+
+<?php
+    // Incrémentation (ajoute 1)
+    $z++;
+    $z = $z + 1; // Exactement le même résultat que la ligne précédente
+    $z += 1; // Exactement le même résultat que la ligne précédente
+
+    // Décrémentation (retire 1)
+    $z--;
+    $z = $z - 1; // Exactement le même résultat que la ligne précédente
+    $z -= 1; // Exactement le même résultat que la ligne précédente
+
+    // Exemple supplémentaire
+    $z = $entier + $entier - $decimal;
+
+    echo $z;
+?>
+
+<h2>Exemple d)</h2>
+<!-- Mise en place d'une liste -->
+<ul>
+    <li><?php echo $texteDuParagraphe; ?></li>
+    <li><?php echo $entier; ?></li>
+    <li><?php echo $decimal; ?></li>
+    <li><?php echo $x; ?></li>
+    <li><?php echo $y; ?></li>
+    <li><?php echo $z; ?></li>
+</ul>
+
 
 </body>
 </html>
